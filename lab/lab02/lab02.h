8414a2b2962f07a9c60757ecd88379d4d219b3df
potterLim <potterLim0808@gmail.com>
2025-05-05 01:00:24 +0900

    chore: add lab02 header file with function declarations

---
+++ lab/lab02/lab02.h
@@ -0,0 +1,11 @@
+#ifndef LAB02_H
+#define LAB02_H
+
+int IsPrime(int n);
+int CountDivisors(int n);
+int SumDivisors(int n);
+int CountOddDivisors(int n);
+int CountEvenDivisors(int n);
+int CountPrimeDivisors(int n);
+
+#endif /* LAB02_H */
