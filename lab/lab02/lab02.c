bec3db2622bbc5f9f90049ddaf4549363ad337b7
potterLim <potterLim0808@gmail.com>
2025-05-05 01:00:36 +0900

    feat: add lab02 implementation template

---
+++ lab/lab02/lab02.c
@@ -0,0 +1,53 @@
+#include "lab02.h"
+
+int is_prime(int number)
+{
+    /*
+    * number가 소수이면 return 1을 소수가 아니면 return 0을 리턴하는 코드를 작성하세요.
+    */
+}
+
+int count_divisors(int number)
+{
+    int count = 0;
+
+    // TODO: 과제를 해결하기 위한 코드를 작성하세요.
+
+    return count;
+}
+
+int sum_divisors(int number)
+{
+    int sum = 0;
+
+    // TODO: 과제를 해결하기 위한 코드를 작성하세요.
+
+    return sum;
+}
+
+int count_odd_divisors(int number)
+{
+    int count = 0;
+
+    // TODO: 과제를 해결하기 위한 코드를 작성하세요.
+
+    return count;
+}
+
+int count_even_divisors(int number)
+{
+    int count = 0;
+
+    // TODO: 과제를 해결하기 위한 코드를 작성하세요.
+
+    return count;
+}
+
+int count_prime_divisors(int number)
+{
+    int count = 0;
+
+    // TODO: 과제를 해결하기 위한 코드를 작성하세요.
+
+    return count;
+}
