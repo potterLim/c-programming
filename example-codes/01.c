ec45d5d99901f344b6c9ac9cd3e8ae474a9cbf18
potterLim <potterLim0808@gmail.com>
2025-05-04 19:28:50 +0900

    feat: add example code for getting started with C

---
+++ example-codes/01.c
@@ -0,0 +1,14 @@
+/*
+ * Example code from: 01-started-with-programming
+ * Purpose          : Prints "Why not Change the World" to the console
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    // Print message to the console
+    printf("Why not Change the World\n");
+
+    return 0;
+}
