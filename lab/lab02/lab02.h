198ff9698167ea2d1e3ea6fbf00bb322a1eb6cf3
potterLim <potterLim0808@gmail.com>
2025-05-05 01:50:03 +0900

    chore: normalize function names in lab02.h to match standard

--- lab/lab02/lab02.h
+++ lab/lab02/lab02.h
@@ -1,11 +1,11 @@
 #ifndef LAB02_H
 #define LAB02_H
 
-int IsPrime(int n);
-int CountDivisors(int n);
-int SumDivisors(int n);
-int CountOddDivisors(int n);
-int CountEvenDivisors(int n);
-int CountPrimeDivisors(int n);
+int is_prime(int number);
+int count_divisors(int number);
+int sum_divisors(int number);
+int count_odd_divisors(int number);
+int count_even_divisors(int number);
+int count_prime_divisors(int number);
 
 #endif /* LAB02_H */
