c5cde9f3d09e587e502590a3aa7d033e033a4729
potterLim <potterLim0808@gmail.com>
2025-05-05 09:01:28 +0900

    feat: add header file with function declarations and include guard

---
+++ example-codes/07/mymath.h
@@ -0,0 +1,13 @@
+#ifndef MYMATH_H
+#define MYMATH_H
+
+// Function prototypes
+int add(int a, int b);
+int subtract(int a, int b);
+int multiply(int a, int b);
+float divide(int a, int b);
+void print_sum(int a, int b);
+void print_difference(int a, int b);
+void modify(int value);  // Shows that parameters are passed by value
+
+#endif  /* MYMATH_H */
