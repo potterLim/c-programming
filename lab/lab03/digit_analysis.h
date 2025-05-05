16bd2b76e0ec0dd0df43a0c26d1e27abce99ccf0
potterLim <potterLim0808@gmail.com>
2025-05-05 11:49:27 +0900

    chore: add lab03 header file with function prototypes

---
+++ lab/lab03/digit_analysis.h
@@ -0,0 +1,11 @@
+#ifndef DIGIT_ANALYSIS_H
+#define DIGIT_ANALYSIS_H
+
+int count_digits_recursive(int n);
+int get_digit_sum_recursive(int n);
+int get_first_digit_recursive(int n);
+int get_last_digit(int n);
+int count_even_digits_recursive(int n);
+int count_odd_digits_recursive(int n);
+
+#endif /* DIGIT_ANALYSIS_H */
