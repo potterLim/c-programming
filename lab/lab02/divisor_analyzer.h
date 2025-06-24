75b5a7f5c96d69239cf3abd9698e66a52920a772
potterLim <potterLim0808@gmail.com>
2025-06-25 07:21:50 +0900

    docs: add concise function descriptions to divisor_analyzer.h

--- lab/lab02/divisor_analyzer.h
+++ lab/lab02/divisor_analyzer.h
@@ -1,11 +1,11 @@
 #ifndef DIVISOR_ANALYZER_H
 #define DIVISOR_ANALYZER_H
 
-int is_prime(int number);
-int count_divisors(int number);
-int sum_divisors(int number);
-int count_odd_divisors(int number);
-int count_even_divisors(int number);
-int count_prime_divisors(int number);
+int is_prime(int number);              // number가 소수인지 아닌지 판별한다.
+int count_divisors(int number);        // number의 약수 개수를 센다.
+int sum_divisors(int number);          // number의 모든 약수를 더한다.
+int count_odd_divisors(int number);    // number의 약수 중에서 홀수인 것의 개수를 센다.
+int count_even_divisors(int number);   // number의 약수 중에서 짝수인 것의 개수를 센다.
+int count_prime_divisors(int number);  // number의 약수 중에서 소수인 것의 개수를 센다.
 
 #endif /* DIVISOR_ANALYZER_H */
